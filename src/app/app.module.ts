b8504e7a165284a00540335e2378c98be6db6422
rajitha109 <rajitha.pussepitiya@randomsoftware.net>
2023-06-28 16:04:34 +0530

    all changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import  { WarningComponent } from './warning/warning.component';
@@ -12,7 +13,8 @@ import { SuccessComponent } from './success/success.component';
     SuccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./success.component.css']
 })
 export class SuccessComponent {
+successId:number =10;
+successName:string="My Success";
+successStatus:string = "Good";
+serverStatus:String ="No server is created";
+allowNewUser = false;
+serverName:String = "TestServer";
+//propery binding - disable property
+
+
+constructor()
+{
+  setTimeout(()=>{
+    this.allowNewUser=true;
+  },2000);  
+}
+
+successStatusfunc()
+{
+  return this.successStatus;
+}
+onServerCreate(){
+  this.serverStatus = "Serve Created "+ this.serverName;
+}
+
+onUpdateServer(event:any){
+  this.serverName = (<HTMLInputElement>event.target).value;
+}
+
+userName:String ="Rajitha";
+EmptyUser=false;
+checkEmptyUserFunc(event:any) {  
+  console.log(this.userName);
+  if (this.userName==""){
+    this.EmptyUser=true;
+  }
+  else
+  {
+    this.EmptyUser=false;
+  }  
+}
+
+updateUserName()
+{
+  this.userName=null;  
+  
+}
 
 }
+
